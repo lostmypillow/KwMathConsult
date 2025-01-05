8046c79c5f24cfa59000678ec98f433ce161dfce
Johnny <47119111+lostmypillow@users.noreply.github.com>
2025-01-05 10:14:35 +0000

    added better update mechanism, add curl to apt and debian specific installation

--- setup.sh
+++ setup.sh
@@ -4,13 +4,15 @@
 set -e
 APP_DIR="$(pwd)"
 SERVICE_NAME="fastapi"
+DISTRO=$(lsb_release -is)
+
 
 echo "SETUP [Update system]"
-sudo apt full-upgrade -qq >/dev/null
+sudo apt-get update >/dev/null
 echo "ok"
 
 echo "SETUP [Ensure necessary packages are installed]"
-sudo apt-get install -y python3-venv python3-pip python3-tk wget p7zip-full >/dev/null
+sudo apt-get install -y python3-venv python3-pip python3-tk wget p7zip-full  curl gnupg >/dev/null
 echo "ok"
 
 
@@ -27,7 +29,16 @@ echo "ok"
 
 echo "SETUP [Install Microsoft ODBC Driver 18]"
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg >/dev/null
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list >/dev/null
+if [ "$DISTRO" == "Ubuntu" ]; then
+    curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list >/dev/null
+
+# Check if the system is Debian
+elif [ "$DISTRO" == "Debian" ];
+    curl https://packages.microsoft.com/config/debian/12/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+else
+    echo "Unsupported distribution. Only Ubuntu and Debian are supported."
+    exit 1
+fi
 sudo apt-get update -y
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18 >/dev/null
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18 >/dev/null
